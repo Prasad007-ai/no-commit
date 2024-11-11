651b06296d9ccec6fd29b595564bc2c2fa2097bf
prasad007 <new- prasaddarji2005@gmail.com>
2024-11-11 14:04:29 +0530

    Initial commit

--- user.php
+++ user.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
             </div>
 
             <div class="form-group">
-                <label>hello</label>
+                <label>password</label>
                 <input type="password" class="form-control" placeholder="enter your password" name="password" autocomplete="off">
             </div>
 
